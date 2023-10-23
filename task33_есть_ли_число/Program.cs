a8c46e1c90863be1805ef88b46e68873928ddd2d
Ellona <el_br10@mail.ru>
2023-10-23 09:57:15 +0300

    Cеминар5 и домашняя работа 5 task34,37,38

---
+++ task33_есть_ли_число/Program.cs
@@ -0,0 +1,161 @@
+﻿/*Задача 33: Задайте массив. Напишите программу, которая
+определяет, присутствует ли заданное число в массиве.
+4; массив [6, 7, 19, 345, 3] -> нет
+-3; массив [6, 7, 19, 345, 3] -> да
+*/ 
+/*Console.Clear();
+int[] GetRandArray(int start, int end, int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i]= new Random().Next(start, end + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write(arr[i]);
+        if(i < arr.Length -1)
+        System.Console.Write(", ");
+    }
+    System.Console.Write("]");
+    System.Console.WriteLine();
+}
+
+bool findNum(int[] array, int num)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(num == array[i])
+        return true;
+    }
+    return false;
+}
+
+System.Console.WriteLine("Ввести размер массива: ");
+int userSize = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Ввести диапазон массива от: ");
+int userStart = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Ввести диапазон массива до: ");
+int userEnd = Convert.ToInt32(Console.ReadLine());
+
+int[] userArray = GetRandArray(userStart, userEnd, userSize);
+PrintArray(userArray);
+System.Console.WriteLine("Какое число ищем?");
+int userNum = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(findNum(userArray,userNum));*/
+
+/*int[] CreateArray(int size, int min, int max)
+{
+    var random = new Random();
+
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = random.Next(min, max);
+    }
+    return arr;
+}
+
+void Output(int[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
+// void IsBelong(int[] arr, int number)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] == number)
+//         System.Console.WriteLine($"Число {number} есть в массиве");
+//         // else if(arr[i]!= number)
+//         // System.Console.WriteLine($"Числа {number} нет в массиве");
+//     }
+// }
+
+bool IsBelong(int[] arr, int number)
+{
+    bool result = false;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == number)
+            result = true;
+    }
+    return result;
+}
+
+int[] arr = CreateArray(4, -21, 10);
+Output(arr);
+
+System.Console.WriteLine("Введите число: ");
+
+int n = int.Parse(Console.ReadLine());
+// IsBelong(arr, n);
+
+// System.Console.WriteLine(IsBelong(arr, n));
+bool result2 = IsBelong(arr,n);
+System.Console.WriteLine((result2)?$"Число {n} в массиве есть":"Числа {n} в массиве нет");*/
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int[] GenerateArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+
+bool findNum(int[] array, int num)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(num == array[i])
+        return true;
+    }
+    return false;
+}
+
+void PrintArray(int[] arrayOne, bool a)
+{
+    System.Console.WriteLine();
+    System.Console.Write("[");
+    for (int i = 0; i < arrayOne.Length; i++)
+    {
+        System.Console.Write(arrayOne[i]);
+        if (i < arrayOne.Length - 1)
+            System.Console.Write(", ");
+    }
+    System.Console.Write("] -> ");
+    if (a == true) System.Console.WriteLine("Да");
+    else System.Console.WriteLine("Нет");
+}
+
+int length = Prompt("Длина массива: ");
+int min = Prompt("Начальное значение для диапазона случайного числа: ");
+int max = Prompt("Конечное значение для дапазона случайного числа: ");
+int num = Prompt("Проверка числа в массиве: ");
+int[] array = GenerateArray(length, min, max);
+PrintArray(array, findNum(array, num));
+
+
+
+
+
---
+++ task37_произведение_пар/Program.cs
@@ -0,0 +1,94 @@
+﻿/*Задача 37: Найдите произведение пар чисел в одномерном массиве.
+Парой считаем первый и последний элемент, второй и предпоследний
+и т.д. Результат запишите в новом массиве.
+[1 2 3 4 5] -> 5 8 3
+[6 7 3 6] -> 12 10
+*/
+
+/*int[] GetRandomArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write(arr[i]);
+        if (i < arr.Length - 1) System.Console.Write(", ");// [5, 7, 1, 4
+
+    }
+
+    System.Console.Write("]");
+}
+
+int[] ProductArray(int[] arr)                // метод для перемножения пар чисел в одномерном массиве 
+                                // и передачи результатов перемножения в новый массив
+{
+    int size=0;                                
+    if (arr.Length%2==1) size=arr.Length/2+1;            // условие, при котором если в исходном массиве нечетное число элементов, 
+    else size=arr.Length/2;                    // чтобы при перемножении пар элементов, начиная с концов массива создать иллюзию
+                                    // перемножения центрального элемента самого на себя
+    int[] res = new int[size];                    // создание нового массива, куда будут записаны результаты перемножения
+    for (int i = 0; i < size; i++)    
+    {
+        if ((arr.Length%2==1)&&(i==size-1)) res[i]=arr[i];    // условие, при котором при нечетном количестве элементов исходного массива 
+        else res[i] = arr[i]*arr[arr.Length-i-1];        // средний его элемент будет приравнен к последнему элементу нового массива
+    }
+    return res;
+}
+int[] userArray = GetRandomArray(7);
+PrintArray(userArray);
+System.Console.WriteLine();
+int[] newArray = ProductArray(userArray);
+PrintArray(newArray);*/
+
+int[] CreateArray(int size, int min, int max)
+{
+    var random = new Random();
+
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = random.Next(min, max);
+    }
+    return arr;
+}
+
+void Output(int[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
+int[] MultArray(int[] array) //[6 7 1 5 6 2 3 8] -> 48 21 2 30
+{
+    int[] result = new int[array.Length - array.Length / 2];
+    int temp = 0;
+    for (int i = 0; i < array.Length - array.Length / 2; i++)
+    {
+        if (i == array.Length - 1 - i)
+        {
+            result[i] = array[i];
+            break; //заканчивает цикл
+        }
+        temp = array[i] array[array.Length - 1 - i]; // первый элемент массива умножается на последний элемент и далее
+        result[i] = temp;
+    }
+    return result;
+}
+
+int[] array = CreateArray(5, 1, 5);
+Output(array);
+System.Console.WriteLine();
+int[] array2 = MultArray(array);
+Output(array2);
